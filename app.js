e29a17367b813fc84f3bcc2bfd672bca0152ea1f
senthilkumarjp <senthilkumarjp55@gmail.com>
2022-12-06 15:53:14 +0530

    3.50

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //REST-Representational state transfer.
-//new
 
+//3.50
 //it is  basically a set of guidelines for how a client + server should communicate and perform CRUD operation on a given resource.
 
 // GET /comments-list all comments
